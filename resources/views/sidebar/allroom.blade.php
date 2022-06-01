ca9864697a4f398b5afbc5ec9335c202106f792a
AnaF <82066444+AnaAzevedo2@users.noreply.github.com>
2022-06-01 19:28:16 +0100

    Update allroom.blade.php

--- resources/views/sidebar/allroom.blade.php
+++ resources/views/sidebar/allroom.blade.php
@@ -40,17 +40,6 @@
                         <li><a href="expenses.html">Expenses </a></li>
                     </ul>
                 </li>
-                <li class="submenu">
-                    <a href="#">
-                        <i class="fa fa-user-plus"></i>
-                        <span> User Management </span>
-                        <span class="menu-arrow"></span>
-                    </a>
-                    <ul class="submenu_class" style="display: none;">
-                        <li><a href="salary.html">All User</a></li>
-                        <li><a href="salary-veiw.html">User Log Activity </a></li>
-                    </ul>
-                </li>
             </ul>
         </div>
     </div>
