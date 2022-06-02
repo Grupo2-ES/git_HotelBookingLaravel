ff754368f64d48e2f0c8eb01e374a333359b0ced
MarioRodrigues2304 <82144702+MarioRodrigues2304@users.noreply.github.com>
2022-06-02 20:35:16 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -98,3 +98,24 @@ Route::post('logout', [UserController::class, 'logout'])->name('logout');
 
 Route::view('/employe', 'employe');
 
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::group(['prefix'=>'admin', 'middleware'=>['isAdmin','auth']], function(){
+        Route::get('dashboard',[AdminController::class,'index'])->name('admin.dashboard');
+        Route::get('profile',[AdminController::class,'profile'])->name('admin.profile');
+        Route::get('settings',[AdminController::class,'settings'])->name('admin.settings');
+
+
+        Route::post('update-profile-info',[AdminController::class,'updateInfo'])->name('adminUpdateInfo');
+        Route::post('change-profile-picture',[AdminController::class,'updatePicture'])->name('adminPictureUpdate');
+        Route::post('change-password',[AdminController::class,'changePassword'])->name('adminChangePassword');
+       
+});
+
+Route::group(['prefix'=>'user', 'middleware'=>['isUser','auth']], function(){
+    Route::get('dashboard',[UserController::class,'index'])->name('user.dashboard');
+    Route::get('profile',[UserController::class,'profile'])->name('user.profile');
+    Route::get('settings',[UserController::class,'settings'])->name('user.settings');
+    
+});
