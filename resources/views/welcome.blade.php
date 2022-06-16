4e66a6e3836ead276fdda89d797c5be639e1acf8
rodmoreira41 <a039291@ismai.pt>
2022-06-16 23:29:41 +0100

    mudanças

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,23 +68,14 @@
     </div>
   </div>
 
-  <!-- Services -->
-  <div class="w3-container" id="services" style="margin-top:75px">
-    <h1 class="w3-xxxlarge w3-text-red"><b>Services.</b></h1>
-    <hr style="width:50px;border:5px solid red" class="w3-round">
-    <p></p>
-    <p>
-    </p>
-  </div>
-  
   <!-- Designers -->
   <div class="w3-container" id="designers" style="margin-top:75px">
     <h1 class="w3-xxxlarge w3-text-red"><b>Members</b></h1>
     <hr style="width:50px;border:5px solid red" class="w3-round">
-    <p>The best team in the world.</p>
+    <p>The best team in the world:</p>
     <p>
     </p>
-    <p><b></b>:</p>
+    <p><b></b></p>
   </div>
 
   <!-- The Team -->
