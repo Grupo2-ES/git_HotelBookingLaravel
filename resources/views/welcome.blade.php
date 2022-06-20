fba4f1f27e9ce9c77159e4b21284c54ccab63b0f
marlope02 <82144508+marlope02@users.noreply.github.com>
2022-06-20 21:23:09 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,9 +25,15 @@
             <div class="hidden fixed top-0 left-0 px-6 py-4 sm:block">
                     <a class="text-sm text-gray-700 dark:text-gray-500 underline" href="{{ url('employee') }}">Funcionários</a>
             </div>
+
+            <P></p>
+
             <div class="hidden fixed top-0 left-0 px-6 py-4 sm:block">
                     <a class="text-sm text-gray-700 dark:text-gray-500 underline" href="{{ url('expenses') }}">Expenses</a>
             </div>
+
+            <P></p>
+
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
                         <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
@@ -61,7 +67,7 @@
     <h1 class="w3-xxxlarge w3-text-red"><b>Projeto em Laravel</b></h1>
     <hr style="width:50px;border:5px solid red" class="w3-round">
   </div>
-  
+
   <!-- Modal for full size images on click-->
   <div id="modal01" class="w3-modal w3-black" style="padding-top:0" onclick="this.style.display='none'">
     <span class="w3-button w3-black w3-xxlarge w3-display-topright">×</span>
@@ -157,7 +163,7 @@
         </li>
       </ul>
     </div>
-        
+
     <div class="w3-half">
       <ul class="w3-ul w3-light-grey w3-center">
         <li class="w3-red w3-xlarge w3-padding-32"></li>
@@ -176,7 +182,7 @@
       </ul>
     </div>
   </div>
-  
+
   <!-- Contact -->
   <div class="w3-container" id="contact" style="margin-top:75px">
     <h1 class="w3-xxxlarge w3-text-red"><b></b></h1>
@@ -196,7 +202,7 @@
         <input class="w3-input w3-border" type="text" name="Message" required>
       </div>
       <button type="submit" class="w3-button w3-block w3-padding-large w3-red w3-margin-bottom">Submit Message</button>
-    </form>  
+    </form>
   </div>
 
 <!-- End page content -->
@@ -207,7 +213,7 @@ function w3_open() {
   document.getElementById("mySidebar").style.display = "block";
   document.getElementById("myOverlay").style.display = "block";
 }
- 
+
 function w3_close() {
   document.getElementById("mySidebar").style.display = "none";
   document.getElementById("myOverlay").style.display = "none";
